a92ea7f4fc6c458c0f7c2d2c5496f9a6bdbb6391
LIZZETH GOMEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2021-06-23 20:55:03 -0500

    Update

--- postwork_S1.R
+++ postwork_S1.R
@@ -13,26 +13,62 @@
   # que jugaron en casa (FTHG) y los goles anotados por los equipos que jugaron como visitante (FTAG)
   
   # 3. Consulta cómo funciona la función table en R al ejecutar en la consola ?table
-  
+
   
   # 1. Cargamos los datos
-  LigaEsp1920 <- "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
+  data.soccer <- "https://www.football-data.co.uk/mmz4281/1920/SP1.csv"
+  download.file(url = data.soccer, destfile = "data.soccer.csv", mode = "wb")
+  data <-  read.csv("data.soccer.csv")
   
-  download.file(url = LigaEsp1920, destfile = "LigaEsp1920.csv", mode = "wb")
+  # 2.Extrae las columnas de goles en casa(FTHG) y visitante(FTAG)
+  data <- data[ ,c(6:7)]
   
-  datainicial <-  read.csv("LigaEsp1920.csv")
-  #2.Extrae las columnas FTHG y FTAG.
-  datainicial<-datainicial[ ,c(6:7)]
-  #3.Elabora tablas de frecuencias relativas.
-  (freqFTHG<-table(datainicial$FTHG))
-  (freqFTAG<-table(datainicial$FTAG))
-  (freqBoth<-table(datainicial))
-  #La probabilidad (marginal) de que el equipo que juega en casa anote x goles
-  (margHome <- freqFTHG / length(datainicial$FTHG))
-  prop.table(freqFTHG) #Mismo resultado
-  #La probabilidad (marginal) de que el equipo que juega como visitante anote x goles
-  (margAway <- freqFTAG / length(datainicial$FTAG))
-  prop.table(freqFTAG) #Mismo resultado
-  #La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el equipo que juega como visitante anote y goles
-  (conj<-freqBoth/380)
-  prop.table(freqBoth)
+  
+  # 3.Elabora tablas de frecuencias relativas.
+  freq_home <-table(data$FTHG)
+  freq_away <-table(data$FTAG)
+  freq_conjunta <-table(data)
+  
+  
+  # Posteriormente elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+  # 1. La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x = 0, 1, 2, ...)
+  # 2. La probabilidad (marginal) de que el equipo que juega como visitante anote y goles (y = 0, 1, 2, ...)
+  # 3. La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y 
+  # el equipo que juega como visitante anote y goles (x = 0, 1, 2, ..., y = 0, 1, 2, ...)
+  
+  
+  # PROBABILIDADES MARGINALES
+  # 1. Probabilidad marginal de goles en casa
+  proba_home <- freq_home / length(data$FTHG)
+  prop.table(proba_home) #Mismo resultado
+  
+  
+  # 2. Probabilidad marginal de goles como visitante
+  proba_away <- freq_away / length(data$FTAG)
+  prop.table(freq_away) #Mismo resultado
+  
+  
+  # Tabla de probailidades de goles en casa y visitante
+  proba_away <- as.data.frame(proba_away)
+  proba_home <- as.data.frame(proba_home)
+  
+  proba_away <- rename(proba_away, goles = Var1, Proba_Away = Freq)
+  proba_home <- rename(proba_home, goles = Var1, Proba_Home = Freq)
+  
+  
+  library(dplyr)
+  tabla <- full_join(proba_home, proba_away, by = "goles")
+  tabla
+  
+  
+  # PROBABILIDAD CONJUNTA
+  # 3. Probabilidad conjunto
+  conj <- freq_conjunta/length(freq_conjunta)
+  names(dimnames(freq_conjunta)) <- c("Home", "Away") 
+  prop.table(freq_conjunta)
+  
+  
+  
+  
+  
+  
