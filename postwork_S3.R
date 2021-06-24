d3f5fcd935f966dcb9651056ca9e62f42ec987fa
LIZZETH GOMEZ <lizzethgomezrodriguez@ciencias.unam.mx>
2021-06-23 21:59:40 -0500

    Update

--- postwork_S3.R
+++ postwork_S3.R
@@ -12,6 +12,7 @@
   # - Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo de casa.
   # - Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo visitante.
   # - Un HeatMap para las probabilidades conjuntas estimadas de los números de goles que anotan el equipo de casa y el equipo visitante en un partido.
+  
   library(ggplot2)
   library(hrbrthemes)
   library(plotly)
@@ -26,7 +27,7 @@
   # Verificamos su estructura
   head(data)
   
-  # Generamos la tabla de contingencia para generar las probabilidades relativas
+  # Generamos la tabla de contingencia para generar las frecuencias relativas
   contingency_table <- table(data[,c("FTHG","FTAG")])
   contingency_table
   
@@ -34,7 +35,9 @@
   contingency_table <- addmargins(contingency_table)
   contingency_table
   
-  # Dividimos cada celda por el total de goles para obtener las probabilidades relativas
+  
+  # 1. PROBABILIDADES MARGINALES
+  # Dividimos cada celda por el total de goles para obtener las probabilidades
   for (i in 1:nrow(contingency_table)){
     for(j in 1:ncol(contingency_table)){
       contingency_table[i,j] <- contingency_table[i,j]/contingency_table["Sum","Sum"]
@@ -42,7 +45,7 @@
   }
   contingency_table
   
-  # 1. Con el último data frame obtenido en el postwork de la sesión 2, elabora tablas de frecuencias relativas para estimar las siguientes probabilidades:
+  
   # La probabilidad (marginal) de que el equipo que juega en casa anote x goles (x=0,1,2,)
   for(i in 1:(nrow(contingency_table)-1)){
     print(paste("Probabilidad marginal de que el equipo en casa anote", i-1, "goles:", contingency_table[i,8]))
@@ -53,16 +56,15 @@
     print(paste("Probabilidad marginal de que el equipo visitante anote ", i-1, "goles:", contingency_table[10,i]))
   }
   
-  # La probabilidad (conjunta) de que el equipo que juega en casa anote x goles y el 
-  # equipo que juega como visitante anote y goles (x=0,1,2,, y=0,1,2,)
+  # La probabilidad conjunta
   for (i in 1:(nrow(contingency_table)-1)){
     for(j in 1:(ncol(contingency_table)-1)){
       print(paste("Probabilidad conjunta de que el equipo en casa anote", i-1, "goles y el equipo visitante anote",j-1,"goles:", contingency_table[i,j]))
     }
   }
   
-  # 2. Realiza lo siguiente:
-  # Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo de casa.
+  # 2. GRAFICOS DE LAS PROBABILIDADES
+  # Un gráfico de goles como visitante
   barplot(contingency_table[-10,"Sum"],
           xlab="Número de goles anotados",
           ylab="Probabilidades",
@@ -70,7 +72,7 @@
           names.arg=rownames(contingency_table[-10,]),
           col="#70D1E0")
   
-  # Un gráfico de barras para las probabilidades marginales estimadas del número de goles que anota el equipo visitante.
+  # Un gráfico grafico de goles en casa
   barplot(contingency_table["Sum",-8],
           xlab="Número de goles anotados",
           ylab="Probabilidades",
