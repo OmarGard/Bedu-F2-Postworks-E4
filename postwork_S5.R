3aeeec72d227de128ad3a9dc0ba6b54bc19e3d81
OmarGard <44001951+OmarGard@users.noreply.github.com>
2021-07-10 13:42:30 -0500

    Update Postwork_S5.R
    Add poisson predictions

--- postwork_S5.R
+++ postwork_S5.R
@@ -29,7 +29,7 @@
 
 
   library(dplyr)
-  
+  library(ggplot2)
   # Cargamos los datos
   df <- read.csv("https://raw.githubusercontent.com/OmarGard/Bedu-F2-Postworks-E4/main/data/postwork_2/D1_17_18_19.csv")
   head(df)
@@ -75,3 +75,58 @@
   
   # 4. Estimacion de probabilidades
   predict(ranking, date = fecha[n])
+  
+  # Poisson
+home <- SmallData[,"home.score"]
+home.pt <- prop.table(table(home))
+home.pt
+
+away <- SmallData[,"away.score"]
+away.pt <- prop.table(table(away))
+away.pt
+
+home.df <- as.data.frame(home.pt)
+away.df <- as.data.frame(away.pt)
+
+poisson_pred.home <- dpois(c(0:8),mean(home))
+poisson_pred.away <- dpois(c(0:6),mean(away))
+
+home.df <- home.df %>%
+  mutate(poisson_pred = poisson_pred.home)
+away.df <- away.df %>%
+  mutate(poisson_pred = poisson_pred.away)
+
+home.df <- home.df %>%
+  mutate(clase = "Home")
+home.df <- home.df %>%
+  rename(
+    goals = home
+  )
+home.df
+away.df <- away.df %>%
+  mutate(clase = "Away")
+away.df <- away.df %>%
+  rename(
+    goals = away
+  )
+away.df
+home_away.df <- rbind(home.df,away.df)
+
+home_away.df %>%
+  ggplot() +
+  geom_col(aes(x = goals, y = Freq, fill = clase),position = "dodge") +
+  geom_line(data=home.df,aes(x=as.numeric(goals) + 0.25,y=poisson_pred), color="#049497") +
+  geom_point(data=home.df,aes(x=as.numeric(goals) + 0.25,y=poisson_pred), color="#049497") +
+  
+
+# ggplot() +
+#   geom_col(data=home_away.df,aes(x = goals, y = Freq, fill = clase),position = "dodge") + 
+#   xlab("Goles anotados") + 
+#   ylab("Proporción de partidos") + 
+#   ggtitle("Frecuencia relativa de goles anotados por equipos en casa y visitantes") +
+#   geom_line(data=home.df,aes(x=(as.numeric(goals) + 0.25),y=poisson_pred, colour="red"), group=1) +
+#   geom_point(data=home.df,aes(x=(as.numeric(goals) + 0.25),y=poisson_pred, colour="red"))
+#   geom_line(data=home.df,aes(x=(as.numeric(goals) + 0.25),y=poisson_pred, colour="red"), group=1) +
+#   geom_point(data=home.df,aes(x=(as.numeric(goals) + 0.25),y=poisson_pred, colour="red"))
+
+
